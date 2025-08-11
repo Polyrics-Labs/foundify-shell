da826571e5982df64be32afb78269353c5a64751
John Mendoza <jhoamen93@hotmail.com>
2025-08-11 11:30:03 -0500

    first configuration module federation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'micro1',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Routes',
+      }).then((m) => m.appHomeRoutes),
   },
   {
     path: '',
