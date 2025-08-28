4f44520d2f09f11a617da580fce6380d0899c4b4
John Mendoza <jhoamen93@hotmail.com>
2025-08-27 22:33:15 -0500

    sync angular version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonToolbar, IonHeader, IonTitle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, IonToolbar, IonHeader, IonTitle],
 })
 export class AppComponent {
   constructor() {}
